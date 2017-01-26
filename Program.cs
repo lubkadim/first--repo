377fd6c644a225e9ec64327f7134c3ca3e1fa3bf
lubkadim <lubka8de@gmail.com>
2017-01-26 14:44:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._6MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int start = 0;
+            int len = 1;
+            int bestLen = 1;
+            int bestStart = 1;
+            for (int pos = 1; pos <= nums.Length - 1; pos++)
+            {
+                if (nums[pos] == nums[pos - 1])
+                {
+                    len++;
+                    if (len > bestLen)
+                    {
+                        bestLen = len;
+                        bestStart = start;                      
+                       
+                    }
+                }
+                else
+                {
+                    start = pos;
+                    len = 1;
+                }
+
+            }
+            for (int i =bestStart; i <bestStart+bestLen; i++)
+            {
+                Console.Write("{0} ",nums[i]);
+            }
+            
+
+        }
+    }
+}
